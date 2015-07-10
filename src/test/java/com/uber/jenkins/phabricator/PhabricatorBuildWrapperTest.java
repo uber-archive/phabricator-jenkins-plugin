8f752541deb75e831dbf6755aef7bdd5504e6007
Aiden Scandella <sc@ndella.com>
2015-07-10 11:14:10 -0600

    Add minimal integration test
    This tests the no-parameter case of both the build wrapper and the
    notifier (publisher). It is not very useful yet, but lays the groundwork
    for testing real flows.

---
+++ src/test/java/com/uber/jenkins/phabricator/PhabricatorBuildWrapperTest.java
@@ -0,0 +1,52 @@
+// Copyright (c) 2015 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package com.uber.jenkins.phabricator;
+
+import hudson.model.FreeStyleBuild;
+import hudson.model.Result;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class PhabricatorBuildWrapperTest extends BuildIntegrationTest {
+    private PhabricatorBuildWrapper wrapper;
+
+    @Before
+    public void setUp() throws IOException {
+        p = createProject();
+        wrapper = new PhabricatorBuildWrapper(
+                false,
+                false,
+                false,
+                true
+        );
+    }
+
+    @Test
+    public void testNoParameterBuild() throws Exception {
+        p.getBuildWrappersList().add(wrapper);
+
+        FreeStyleBuild build = p.scheduleBuild2(0).get();
+        Result result = build.getResult();
+        assertSuccessfulBuild(result);
+    }
+}
