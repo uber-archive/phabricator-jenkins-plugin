uber-archive/phabricator-jenkins-plugin

commit c9ca27b95710358fed14ae5665c2c618d4e5d4e1

Break Differential up to make it testable
Previously was mixing concerns with both talking to arcanist and
presenting results. By breaking out a DifferentialClient that I can
easily mock, we can test the core functionality without a ton of boilerplate.

src/main/java/com/uber/jenkins/phabricator/PhabricatorBuildWrapper.java
@@ -24,6 +24,7 @@ import com.uber.jenkins.phabricator.conduit.ArcanistClient;
 import com.uber.jenkins.phabricator.conduit.ArcanistUsageException;
 import com.uber.jenkins.phabricator.conduit.Differential;
 import com.uber.jenkins.phabricator.utils.CommonUtils;
+import com.uber.jenkins.phabricator.conduit.DifferentialClient;
 import hudson.EnvVars;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
@@ -89,16 +90,17 @@ public class PhabricatorBuildWrapper extends BuildWrapper {
                 }
             }
 
+            DifferentialClient diffClient = new DifferentialClient(diffID, starter, conduitToken, arcPath);
             Differential diff;
             try {
-                diff = new Differential(diffID, starter, conduitToken, arcPath);
+                diff = new Differential(diffClient);
                 diff.decorate(build, this.getPhabricatorURL());
 
                 logger.println("Applying patch for differential");
 
                 // Post a silent notification if option is enabled
                 if (showBuildStartedMessage) {
-                    diff.postComment(diff.getBuildStartedMessage(environment));
+                    diffClient.postComment(diff.getBuildStartedMessage(environment));
                 }
             } catch (ArcanistUsageException e) {
                 logger.println("[arcanist] unable to apply patch");

src/main/java/com/uber/jenkins/phabricator/PhabricatorNotifier.java
@@ -23,6 +23,7 @@ package com.uber.jenkins.phabricator;
 import com.uber.jenkins.phabricator.conduit.ArcanistUsageException;
 import com.uber.jenkins.phabricator.conduit.Differential;
 
+import com.uber.jenkins.phabricator.conduit.DifferentialClient;
 import com.uber.jenkins.phabricator.uberalls.UberallsClient;
 import com.uber.jenkins.phabricator.utils.CommonUtils;
 import com.uber.jenkins.phabricator.utils.Logger;
@@ -117,15 +118,16 @@ public class PhabricatorNotifier extends Notifier {
 
         LauncherFactory starter = new LauncherFactory(launcher, environment, listener.getLogger(), build.getWorkspace());
 
+        DifferentialClient diffClient = new DifferentialClient(diffID, starter, conduitToken, arcPath);
         Differential diff;
         try {
-            diff = new Differential(diffID, starter, conduitToken, arcPath);
+            diff = new Differential(diffClient);
         } catch (ArcanistUsageException e) {
             logger.info("arcanist", "unable to fetch differential");
             return true;
         }
 
-        String revisionID = diff.getRevisionID();
+        String revisionID = diff.getRevisionID(true);
         if (CommonUtils.isBlank(revisionID)) {
             return this.ignoreBuild(logger.getStream(), "Unable to load revisionID from conduit for diff ID " + diffID);
         }
@@ -172,7 +174,7 @@ public class PhabricatorNotifier extends Notifier {
         if (runHarbormaster) {
             logger.info("uberalls", "Sending build result to Harbormaster with PHID '" + phid + "', success: " + harbormasterSuccess);
             try {
-                diff.harbormaster(phid, harbormasterSuccess);
+                diffClient.harbormaster(phid, harbormasterSuccess);
             } catch (ArcanistUsageException e) {
                 logger.info("arcanist", "unable to post to harbormaster");
                 return true;
@@ -214,7 +216,7 @@ public class PhabricatorNotifier extends Notifier {
 
             JSONObject result = null;
             try {
-                result = diff.postComment(comment, silent, commentAction);
+                result = diffClient.postComment(comment, silent, commentAction);
             } catch (ArcanistUsageException e) {
                 logger.info("arcanist", "unable to post comment");
             }
@@ -222,7 +224,7 @@ public class PhabricatorNotifier extends Notifier {
                 logger.info("arcanist", "Get error " + result.get("errorMessage") + " with action " +
                         commentAction + "; trying again with action 'none'");
                 try {
-                    diff.postComment(comment, silent, "none");
+                    diffClient.postComment(comment, silent, "none");
                 } catch (ArcanistUsageException e) {
                     logger.info("arcanist", "unable to post comment");
                 }

src/main/java/com/uber/jenkins/phabricator/conduit/DifferentialClient.java
@@ -1,0 +1,122 @@
+// Copyright (c) 2015 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package com.uber.jenkins.phabricator.conduit;
+
+import com.uber.jenkins.phabricator.LauncherFactory;
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * DifferentialClient handles all interaction with conduit/arc for differentials
+ */
+public class DifferentialClient {
+    private final String arcPath;
+    private final String conduitToken;
+    private final LauncherFactory launcher;
+    private final String diffID;
+
+    public DifferentialClient(String diffID, LauncherFactory launcher, String arcPath, String conduitToken) {
+        this.diffID = diffID;
+        this.launcher = launcher;
+        this.arcPath = arcPath;
+        this.conduitToken = conduitToken;
+    }
+
+    /**
+     * Posts a comment to a differential
+     * @param message
+     * @param silent whether or not to trigger an email
+     * @param action phabricator comment action, e.g. 'resign', 'reject', 'none'
+     */
+    public JSONObject postComment(String message, boolean silent, String action) throws IOException, InterruptedException, ArcanistUsageException {
+        Map params = new HashMap<String, String>();
+        params.put("revision_id", this.diffID);
+        params.put("action", action);
+        params.put("message", message);
+        params.put("silent", silent);
+
+        return this.callConduit("differential.createcomment", params);
+    }
+
+    public JSONObject fetchDiff() throws ArcanistUsageException, IOException, InterruptedException {
+        Map params = new HashMap<String, String>();
+        params.put("ids", new String[]{this.diffID});
+        JSONObject query = this.callConduit("differential.querydiffs", params);
+        JSONObject response;
+        try {
+            response = query.getJSONObject("response");
+        } catch (JSONException e) {
+            e.printStackTrace();
+            throw new ArcanistUsageException(
+                    String.format("No 'response' object found in conduit call: (%s) %s",
+                            e.getMessage(),
+                            query.toString(2)));
+        }
+        try {
+            return response.getJSONObject(diffID);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            throw new ArcanistUsageException(
+                    String.format("Unable to find '%s' key in response: (%s) %s",
+                            diffID,
+                            e.getMessage(),
+                            response.toString(2)));
+
+        }
+    }
+
+    protected JSONObject callConduit(String methodName, Map<String, String> params) throws IOException, InterruptedException, ArcanistUsageException {
+        ArcanistClient arc = new ArcanistClient(this.arcPath, "call-conduit", params, this.conduitToken, methodName);
+        return arc.parseConduit(this.launcher.launch(), this.launcher.getStderr());
+    }
+
+    /**
+     * Sets a harbormaster build status
+     * @param phid Phabricator object ID
+     * @param pass whether or not the build passed
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public void harbormaster(String phid, boolean pass) throws IOException, InterruptedException, ArcanistUsageException {
+        Map params = new HashMap<String, String>();
+        params.put("type", pass ? "pass" : "fail");
+        params.put("buildTargetPHID", phid);
+
+        this.callConduit("harbormaster.sendmessage", params);
+    }
+
+
+    /**
+     * Post a comment on the differential
+     * @param message the string message to post
+     * @return
+     * @throws IOException
+     * @throws InterruptedException
+     * @throws ArcanistUsageException
+     */
+    public JSONObject postComment(String message) throws IOException, InterruptedException, ArcanistUsageException {
+        return postComment(message, true, "none");
+    }
+}

phabricator-plugin.iml
@@ -7,6 +7,7 @@
       <sourceFolder url="file://$MODULE_DIR$/src/main/java" isTestSource="false" />
       <sourceFolder url="file://$MODULE_DIR$/src/main/resources" type="java-resource" />
       <sourceFolder url="file://$MODULE_DIR$/src/test/java" isTestSource="true" />
+      <sourceFolder url="file://$MODULE_DIR$/src/test/resources" type="java-test-resource" />
       <excludeFolder url="file://$MODULE_DIR$/target" />
     </content>
     <orderEntry type="inheritedJdk" />
@@ -121,6 +122,10 @@
     <orderEntry type="library" name="Maven: net.sourceforge.nekohtml:nekohtml:1.9.13" level="project" />
     <orderEntry type="library" name="Maven: net.sf.trove4j:trove4j:3.0.3" level="project" />
     <orderEntry type="library" name="Maven: net.java.dev.jna:jna:3.2.2" level="project" />
+    <orderEntry type="library" scope="TEST" name="Maven: org.mockito:mockito-core:2.0.23-beta" level="project" />
+    <orderEntry type="library" scope="TEST" name="Maven: net.bytebuddy:byte-buddy:0.6.11" level="project" />
+    <orderEntry type="library" scope="PROVIDED" name="Maven: org.hamcrest:hamcrest-core:1.1" level="project" />
+    <orderEntry type="library" scope="TEST" name="Maven: org.objenesis:objenesis:2.1" level="project" />
     <orderEntry type="library" scope="TEST" name="Maven: org.jenkins-ci.main:jenkins-war:war:1.597" level="project" />
     <orderEntry type="library" scope="PROVIDED" name="Maven: org.jenkins-ci.main:jenkins-core:1.597" level="project" />
     <orderEntry type="library" scope="PROVIDED" name="Maven: org.jenkins-ci.plugins.icon-shim:icon-set:1.0.5" level="project" />
@@ -259,7 +264,6 @@
     <orderEntry type="library" scope="TEST" name="Maven: org.jenkins-ci:test-annotations:1.1" level="project" />
     <orderEntry type="library" scope="TEST" name="Maven: org.jvnet.mock-javamail:mock-javamail:1.7" level="project" />
     <orderEntry type="library" scope="PROVIDED" name="Maven: junit:junit:4.11" level="project" />
-    <orderEntry type="library" scope="PROVIDED" name="Maven: org.hamcrest:hamcrest-core:1.3" level="project" />
     <orderEntry type="library" scope="TEST" name="Maven: org.hamcrest:hamcrest-library:1.3" level="project" />
     <orderEntry type="library" scope="TEST" name="Maven: org.jenkins-ci:htmlunit:2.6-jenkins-6" level="project" />
     <orderEntry type="library" scope="TEST" name="Maven: org.jvnet.hudson:htmlunit-core-js:2.6-hudson-1" level="project" />

pom.xml
@@ -44,6 +44,7 @@
   </pluginRepositories>
 
   <dependencies>
+    <!-- production dependencies -->
     <dependency>
       <groupId>org.apache.httpcomponents</groupId>
       <artifactId>httpclient</artifactId>
@@ -67,6 +68,14 @@
         <groupId>net.java.dev.jna</groupId>
         <artifactId>jna</artifactId>
         <version>3.2.2</version>
+    </dependency>
+
+    <!-- test dependencies -->
+    <dependency>
+      <scope>test</scope>
+      <groupId>org.mockito</groupId>
+      <artifactId>mockito-core</artifactId>
+      <version>2.0.23-beta</version>
     </dependency>
   </dependencies>
 

src/main/java/com/uber/jenkins/phabricator/conduit/Differential.java
@@ -25,7 +25,6 @@ import com.uber.jenkins.phabricator.PhabricatorPostbuildAction;
 import com.uber.jenkins.phabricator.PhabricatorPostbuildSummaryAction;
 import hudson.EnvVars;
 import hudson.model.AbstractBuild;
-import hudson.model.Result;
 import net.sf.json.JSONException;
 import net.sf.json.JSONNull;
 import net.sf.json.JSONObject;
@@ -33,44 +32,15 @@ import net.sf.json.JSONObject;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Differential {
-    private final JSONObject rawJSON;
-    private final LauncherFactory launcher;
-    private final String conduitToken;
-    private final String arcPath;
+    private JSONObject rawJSON;
 
-    public Differential(String diffID, LauncherFactory launcher, String conduitToken, String arcPath) throws IOException, InterruptedException, ArcanistUsageException {
-        this.conduitToken = conduitToken;
-        this.arcPath = arcPath;
-        this.launcher = launcher;
-        Map params = new HashMap<String, String>();
-        params.put("ids", new String[]{diffID});
+    private final DifferentialClient client;
 
-        JSONObject query = this.callConduit("differential.querydiffs", params);
-        JSONObject response;
-        try {
-            response = query.getJSONObject("response");
-        } catch (JSONException e) {
-            e.printStackTrace();
-            throw new ArcanistUsageException(
-                    String.format("No 'response' object found in conduit call: (%s) %s",
-                            e.getMessage(),
-                            query.toString(2)));
-        }
-        try {
-            this.rawJSON = response.getJSONObject(diffID);
-        } catch (JSONException e) {
-            e.printStackTrace();
-            throw new ArcanistUsageException(
-                    String.format("Unable to find '%s' key in response: (%s) %s",
-                            diffID,
-                            e.getMessage(),
-                            response.toString(2)));
-
-        }
+    public Differential(DifferentialClient client) throws ArcanistUsageException, IOException, InterruptedException {
+        this.client = client;
+        this.rawJSON = client.fetchDiff();
     }
 
     public String getRevisionID(boolean formatted) {
@@ -82,50 +52,6 @@ public class Differential {
             return String.format("D%s", rawRevisionId);
         }
         return rawRevisionId;
-    }
-
-    public String getRevisionID() {
-        return this.getRevisionID(true);
-    }
-
-    /**
-     * Sets a harbormaster build status
-     * @param phid Phabricator object ID
-     * @param pass whether or not the build passed
-     * @throws IOException
-     * @throws InterruptedException
-     */
-    public void harbormaster(String phid, boolean pass) throws IOException, InterruptedException, ArcanistUsageException {
-        Map params = new HashMap<String, String>();
-        params.put("type", pass ? "pass" : "fail");
-        params.put("buildTargetPHID", phid);
-
-        this.callConduit("harbormaster.sendmessage", params);
-    }
-
-    private JSONObject callConduit(String methodName, Map<String, String> params) throws IOException, InterruptedException, ArcanistUsageException {
-        ArcanistClient arc = new ArcanistClient(this.arcPath, "call-conduit", params, this.conduitToken, methodName);
-        return arc.parseConduit(this.launcher.launch(), this.launcher.getStderr());
-    }
-
-    /**
-     * Posts a comment to a differential
-     * @param message
-     * @param silent whether or not to trigger an email
-     * @param action phabricator comment action, e.g. 'resign', 'reject', 'none'
-     */
-    public JSONObject postComment(String message, boolean silent, String action) throws IOException, InterruptedException, ArcanistUsageException {
-        Map params = new HashMap<String, String>();
-        params.put("revision_id", this.getRevisionID(false));
-        params.put("action", action);
-        params.put("message", message);
-        params.put("silent", silent);
-
-        return this.callConduit("differential.createcomment", params);
-    }
-
-    public JSONObject postComment(String message) throws IOException, InterruptedException, ArcanistUsageException {
-        return postComment(message, true, "none");
     }
 
     /**

src/test/java/com/uber/jenkins/phabricator/conduit/DifferentialTest.java
@@ -1,0 +1,80 @@
+// Copyright (c) 2015 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package com.uber.jenkins.phabricator.conduit;
+
+
+import hudson.EnvVars;
+import junit.framework.TestCase;
+import net.sf.json.JSONObject;
+import net.sf.json.groovy.JsonSlurper;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class DifferentialTest extends TestCase {
+    private static final String fakeDiffID = "not-a-real-id";
+    @Rule
+    Differential d;
+
+    public DifferentialTest() throws InterruptedException, ArcanistUsageException, IOException {
+        d = diffWithResponse();
+    }
+
+    @Test
+    public void testFetchRevisionID() throws Exception {
+        assertEquals(fakeDiffID, d.getRevisionID(false));
+    }
+
+    @Test
+    public void testGetPhabricatorLink() throws Exception {
+        assertTrue(d.getPhabricatorLink("http://example.com").contains(fakeDiffID));
+    }
+
+    @Test
+    public void testGetPhabricatorLinkInvalidURL() throws Exception {
+        // Try our best to join URLs, even when they are wrong
+        assertTrue(d.getPhabricatorLink("aoeu").contains("aoeu"));
+    }
+
+    @Test
+    public void testGetBuildStartedMessage() throws Exception {
+        assertTrue(d.getBuildStartedMessage(new EnvVars()).contains("Build started"));
+    }
+
+    private Differential diffWithResponse() throws InterruptedException, ArcanistUsageException, IOException {
+        DifferentialClient client = mock(DifferentialClient.class);
+        when(client.fetchDiff()).thenReturn(getValidQueryResponse());
+
+        return new Differential(client);
+    }
+
+    private JSONObject getValidQueryResponse() throws IOException {
+        InputStream input = getClass().getResourceAsStream(
+                "validDifferentialQueryResponse.json"
+        );
+        return (JSONObject) new JsonSlurper().parse(input);
+    }
+
+}

commit 1d9752801f4644d6cd9b34c5e73a9ef473f5a83e

Fix regression in posting comments
Introduced in c9ca27b, I was using the "diff" ID instead of the "revision" ID
when trying to post a comment.

I'm not super happy about the separation of concerns here and am open to
alternate approaches / suggestions for improvement.

I thought about making DifferentialClient fetch the Differential on
instantiation and store a reference, then calling `getRevisionID(false)`
internally when it wants to `postComment`, but that felt like unnecessary
coupling, and I didn't like the side-effect of fetching on instantiation.

src/main/java/com/uber/jenkins/phabricator/PhabricatorBuildWrapper.java
@@ -100,7 +100,7 @@ public class PhabricatorBuildWrapper extends BuildWrapper {
 
                 // Post a silent notification if option is enabled
                 if (showBuildStartedMessage) {
-                    diffClient.postComment(diff.getBuildStartedMessage(environment));
+                    diffClient.postComment(diff.getRevisionID(false), diff.getBuildStartedMessage(environment));
                 }
             } catch (ArcanistUsageException e) {
                 logger.println("[arcanist] unable to apply patch");

src/main/java/com/uber/jenkins/phabricator/PhabricatorNotifier.java
@@ -38,8 +38,6 @@ import hudson.plugins.cobertura.CoberturaBuildAction;
 import hudson.plugins.cobertura.targets.CoverageResult;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Notifier;
-import net.sf.json.JSONNull;
-import net.sf.json.JSONObject;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 import java.io.IOException;
@@ -177,7 +175,7 @@ public class PhabricatorNotifier extends Notifier {
                 commenter.addBuildLink();
             }
 
-            new PostCommentTask(logger, diffClient, commenter.getComment(), commentAction).run();
+            new PostCommentTask(logger, diffClient, diff.getRevisionID(false), commenter.getComment(), commentAction).run();
         }
 
         return true;

src/main/java/com/uber/jenkins/phabricator/conduit/DifferentialClient.java
@@ -50,9 +50,9 @@ public class DifferentialClient {
      * @param silent whether or not to trigger an email
      * @param action phabricator comment action, e.g. 'resign', 'reject', 'none'
      */
-    public JSONObject postComment(String message, boolean silent, String action) throws IOException, InterruptedException, ArcanistUsageException {
+    public JSONObject postComment(String revisionID, String message, boolean silent, String action) throws IOException, InterruptedException, ArcanistUsageException {
         Map params = new HashMap<String, String>();
-        params.put("revision_id", this.diffID);
+        params.put("revision_id", revisionID);
         params.put("action", action);
         params.put("message", message);
         params.put("silent", silent);
@@ -108,8 +108,8 @@ public class DifferentialClient {
      * @throws InterruptedException
      * @throws ArcanistUsageException
      */
-    public JSONObject postComment(String message) throws IOException, InterruptedException, ArcanistUsageException {
-        return postComment(message, true, "none");
+    public JSONObject postComment(String revisionID, String message) throws IOException, InterruptedException, ArcanistUsageException {
+        return postComment(revisionID, message, true, "none");
     }
 
     protected JSONObject callConduit(String methodName, Map<String, String> params) throws IOException, InterruptedException, ArcanistUsageException {

src/main/java/com/uber/jenkins/phabricator/tasks/PostCommentTask.java
@@ -36,19 +36,21 @@ public class PostCommentTask extends Task {
     private static final boolean SILENT = false;
     private static final String DEFAULT_COMMENT_ACTION = "none";
 
-    private DifferentialClient differentialClient;
-    private String commentAction;
-    private String comment;
+    private final DifferentialClient differentialClient;
+    private final String revisionID;
+    private final String comment;
+    private final String commentAction;
 
     /**
      * PostCommentTask constructor.
      * @param logger The logger.
      */
     public PostCommentTask(Logger logger, DifferentialClient differentialClient,
-                           String comment, String commentAction) {
+                           String revisionID, String comment, String commentAction) {
         super(logger);
 
         this.differentialClient = differentialClient;
+        this.revisionID = revisionID;
         this.comment = comment;
         this.commentAction = commentAction;
     }
@@ -98,8 +100,8 @@ public class PostCommentTask extends Task {
 
     private JSONObject postDifferentialComment(String message, boolean silent, String action) {
         try {
-            JSONObject postDifferentialCommentResult = differentialClient.postComment(message,
-                    silent, action);
+            JSONObject postDifferentialCommentResult = differentialClient.postComment(revisionID,
+                    message, silent, action);
             result = Result.SUCCESS;
             return postDifferentialCommentResult;
         } catch (ArcanistUsageException e) {

src/test/java/com/uber/jenkins/phabricator/conduit/DifferentialClientTest.java
@@ -20,7 +20,6 @@
 
 package com.uber.jenkins.phabricator.conduit;
 
-import com.uber.jenkins.phabricator.LauncherFactory;
 import com.uber.jenkins.phabricator.utils.TestUtils;
 import net.sf.json.JSONObject;
 import org.junit.Before;
@@ -32,7 +31,7 @@ import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyMap;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
 
 public class DifferentialClientTest {
 
@@ -50,7 +49,7 @@ public class DifferentialClientTest {
 
         mockConduitResponse(differentialClient, sentinel);
 
-        JSONObject response = differentialClient.postComment("hello", true, "none");
+        JSONObject response = differentialClient.postComment("anything", "hello", true, "none");
         assertEquals(sentinel, response);
     }
 
@@ -61,7 +60,7 @@ public class DifferentialClientTest {
 
         mockConduitResponse(differentialClient, sentinel);
 
-        JSONObject response = differentialClient.postComment("hello");
+        JSONObject response = differentialClient.postComment("anything", "hello");
         assertEquals(response, sentinel);
     }
 

src/test/java/com/uber/jenkins/phabricator/tasks/PostCommentTaskTest.java
@@ -20,19 +20,18 @@
 
 package com.uber.jenkins.phabricator.tasks;
 
-import com.uber.jenkins.phabricator.CodeCoverageMetrics;
 import com.uber.jenkins.phabricator.conduit.ArcanistUsageException;
 import com.uber.jenkins.phabricator.conduit.DifferentialClient;
-import com.uber.jenkins.phabricator.uberalls.UberallsClient;
 import com.uber.jenkins.phabricator.utils.Logger;
 import com.uber.jenkins.phabricator.utils.TestUtils;
 import net.sf.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 public class PostCommentTaskTest {
 
@@ -41,6 +40,7 @@ public class PostCommentTaskTest {
 
     private String TEST_COMMENT = "They are selling like hotcakes!";
     private String TEST_COMMENT_ACTION = "none";
+    private String TEST_REVISION_ID = "something";
 
     @Before
     public void setup() {
@@ -52,12 +52,13 @@ public class PostCommentTaskTest {
     public void testPostDifferentialFailed() throws Exception {
         doThrow(new ArcanistUsageException("")).when(differentialClient).postComment(
                 anyString(),
+                anyString(),
                 anyBoolean(),
                 anyString()
         );
 
-        PostCommentTask postCommentTask = new PostCommentTask(logger, differentialClient, TEST_COMMENT,
-                TEST_COMMENT_ACTION);
+        PostCommentTask postCommentTask = new PostCommentTask(logger, differentialClient,
+                TEST_REVISION_ID, TEST_COMMENT, TEST_COMMENT_ACTION);
         Task.Result result = postCommentTask.run();
         assert result == Task.Result.FAILURE;
     }
@@ -66,11 +67,12 @@ public class PostCommentTaskTest {
     public void testPostDifferentialSuccess() throws Exception {
         doReturn(new JSONObject()).when(differentialClient).postComment(
                 anyString(),
+                anyString(),
                 anyBoolean(),
                 anyString()
         );
 
-        assert new PostCommentTask(logger, differentialClient, TEST_COMMENT,
-                TEST_COMMENT_ACTION).run() == Task.Result.SUCCESS;
+        assert new PostCommentTask(logger, differentialClient, TEST_REVISION_ID,
+                TEST_COMMENT, TEST_COMMENT_ACTION).run() == Task.Result.SUCCESS;
     }
 }