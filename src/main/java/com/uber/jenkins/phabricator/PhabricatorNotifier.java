uber-archive/phabricator-jenkins-plugin

commit 1762407c198b11cfcf19262e3e918c00d11ce88b

address feedback

src/main/java/com/uber/jenkins/phabricator/PhabricatorNotifier.java
@@ -121,7 +121,7 @@ public class PhabricatorNotifier extends Notifier {
         DifferentialClient diffClient = new DifferentialClient(diffID, starter, conduitToken, arcPath);
         Differential diff;
         try {
-            diff = new Differential(diffClient);
+            diff = new Differential(diffClient.fetchDiff());
         } catch (ArcanistUsageException e) {
             logger.info("arcanist", "unable to fetch differential");
             return true;
@@ -174,9 +174,9 @@ public class PhabricatorNotifier extends Notifier {
         if (runHarbormaster) {
             logger.info("uberalls", "Sending build result to Harbormaster with PHID '" + phid + "', success: " + harbormasterSuccess);
             try {
-                diffClient.harbormaster(phid, harbormasterSuccess);
+                diffClient.sendHarbormasterMessage(phid, harbormasterSuccess);
             } catch (ArcanistUsageException e) {
-                logger.info("arcanist", "unable to post to harbormaster");
+                logger.info("arcanist", "unable to post to sendHarbormasterMessage");
                 return true;
             }
         } else {

src/main/java/com/uber/jenkins/phabricator/conduit/DifferentialClient.java
@@ -68,7 +68,6 @@ public class DifferentialClient {
         try {
             response = query.getJSONObject("response");
         } catch (JSONException e) {
-            e.printStackTrace();
             throw new ArcanistUsageException(
                     String.format("No 'response' object found in conduit call: (%s) %s",
                             e.getMessage(),
@@ -77,7 +76,6 @@ public class DifferentialClient {
         try {
             return response.getJSONObject(diffID);
         } catch (JSONException e) {
-            e.printStackTrace();
             throw new ArcanistUsageException(
                     String.format("Unable to find '%s' key in response: (%s) %s",
                             diffID,
@@ -87,24 +85,19 @@ public class DifferentialClient {
         }
     }
 
-    protected JSONObject callConduit(String methodName, Map<String, String> params) throws IOException, InterruptedException, ArcanistUsageException {
-        ArcanistClient arc = new ArcanistClient(this.arcPath, "call-conduit", params, this.conduitToken, methodName);
-        return arc.parseConduit(this.launcher.launch(), this.launcher.getStderr());
-    }
-
     /**
-     * Sets a harbormaster build status
+     * Sets a sendHarbormasterMessage build status
      * @param phid Phabricator object ID
      * @param pass whether or not the build passed
      * @throws IOException
      * @throws InterruptedException
      */
-    public void harbormaster(String phid, boolean pass) throws IOException, InterruptedException, ArcanistUsageException {
+    public JSONObject sendHarbormasterMessage(String phid, boolean pass) throws IOException, InterruptedException, ArcanistUsageException {
         Map params = new HashMap<String, String>();
         params.put("type", pass ? "pass" : "fail");
         params.put("buildTargetPHID", phid);
 
-        this.callConduit("harbormaster.sendmessage", params);
+        return this.callConduit("sendHarbormasterMessage.sendmessage", params);
     }
 
 
@@ -119,4 +112,10 @@ public class DifferentialClient {
     public JSONObject postComment(String message) throws IOException, InterruptedException, ArcanistUsageException {
         return postComment(message, true, "none");
     }
+
+    protected JSONObject callConduit(String methodName, Map<String, String> params) throws IOException, InterruptedException, ArcanistUsageException {
+        ArcanistClient arc = new ArcanistClient(this.arcPath, "call-conduit", params, this.conduitToken, methodName);
+        return arc.parseConduit(this.launcher.launch(), this.launcher.getStderr());
+    }
+
 }

src/main/java/com/uber/jenkins/phabricator/PhabricatorBuildWrapper.java
@@ -93,7 +93,7 @@ public class PhabricatorBuildWrapper extends BuildWrapper {
             DifferentialClient diffClient = new DifferentialClient(diffID, starter, conduitToken, arcPath);
             Differential diff;
             try {
-                diff = new Differential(diffClient);
+                diff = new Differential(diffClient.fetchDiff());
                 diff.decorate(build, this.getPhabricatorURL());
 
                 logger.println("Applying patch for differential");

src/main/java/com/uber/jenkins/phabricator/conduit/Differential.java
@@ -20,12 +20,10 @@
 
 package com.uber.jenkins.phabricator.conduit;
 
-import com.uber.jenkins.phabricator.LauncherFactory;
 import com.uber.jenkins.phabricator.PhabricatorPostbuildAction;
 import com.uber.jenkins.phabricator.PhabricatorPostbuildSummaryAction;
 import hudson.EnvVars;
 import hudson.model.AbstractBuild;
-import net.sf.json.JSONException;
 import net.sf.json.JSONNull;
 import net.sf.json.JSONObject;
 
@@ -36,11 +34,9 @@ import java.net.URL;
 public class Differential {
     private JSONObject rawJSON;
 
-    private final DifferentialClient client;
 
-    public Differential(DifferentialClient client) throws ArcanistUsageException, IOException, InterruptedException {
-        this.client = client;
-        this.rawJSON = client.fetchDiff();
+    public Differential(JSONObject rawJSON) throws ArcanistUsageException, IOException, InterruptedException {
+        this.rawJSON = rawJSON;
     }
 
     public String getRevisionID(boolean formatted) {

src/test/java/com/uber/jenkins/phabricator/conduit/DifferentialTest.java
@@ -20,12 +20,11 @@
 
 package com.uber.jenkins.phabricator.conduit;
 
-
 import hudson.EnvVars;
 import junit.framework.TestCase;
 import net.sf.json.JSONObject;
 import net.sf.json.groovy.JsonSlurper;
-import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.*;
@@ -34,40 +33,41 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class DifferentialTest extends TestCase {
-    private static final String fakeDiffID = "not-a-real-id";
-    @Rule
-    Differential d;
+    private static final String FAKE_DIFF_ID = "not-a-real-id";
 
-    public DifferentialTest() throws InterruptedException, ArcanistUsageException, IOException {
-        d = diffWithResponse();
+    Differential differential;
+
+    @Before
+    public void setupDiff () throws InterruptedException, ArcanistUsageException, IOException {
+        differential = diffWithResponse();
     }
 
     @Test
     public void testFetchRevisionID() throws Exception {
-        assertEquals(fakeDiffID, d.getRevisionID(false));
+        assertEquals(FAKE_DIFF_ID, differential.getRevisionID(false));
     }
 
     @Test
     public void testGetPhabricatorLink() throws Exception {
-        assertTrue(d.getPhabricatorLink("http://example.com").contains(fakeDiffID));
+        assertTrue(differential.getPhabricatorLink("http://example.com").contains(FAKE_DIFF_ID));
     }
 
     @Test
     public void testGetPhabricatorLinkInvalidURL() throws Exception {
         // Try our best to join URLs, even when they are wrong
-        assertTrue(d.getPhabricatorLink("aoeu").contains("aoeu"));
+        assertTrue(differential.getPhabricatorLink("aoeu").contains("aoeu"));
     }
 
     @Test
     public void testGetBuildStartedMessage() throws Exception {
-        assertTrue(d.getBuildStartedMessage(new EnvVars()).contains("Build started"));
+        assertTrue(differential.getBuildStartedMessage(new EnvVars()).contains("Build started"));
     }
 
     private Differential diffWithResponse() throws InterruptedException, ArcanistUsageException, IOException {
         DifferentialClient client = mock(DifferentialClient.class);
         when(client.fetchDiff()).thenReturn(getValidQueryResponse());
 
-        return new Differential(client);
+        return new Differential(client.fetchDiff());
     }
 
     private JSONObject getValidQueryResponse() throws IOException {

commit 0a868de42e6336db2ab0c302a33baf1da706e40b

Fix harbormaster regression
Fallout from automatic refactor in 1762407c198b11cfcf19262e3e918c00d11ce88b

src/main/java/com/uber/jenkins/phabricator/PhabricatorNotifier.java
@@ -38,6 +38,8 @@ import hudson.plugins.cobertura.CoberturaBuildAction;
 import hudson.plugins.cobertura.targets.CoverageResult;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Notifier;
+import net.sf.json.JSONNull;
+import net.sf.json.JSONObject;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 import java.io.IOException;
@@ -141,11 +143,16 @@ public class PhabricatorNotifier extends Notifier {
 
         String commentAction = "none";
         if (runHarbormaster) {
-            logger.info("uberalls", "Sending build result to Harbormaster with PHID '" + phid + "', success: " + harbormasterSuccess);
+            logger.info("harbormaster", "Sending build result to Harbormaster with PHID '" + phid + "', success: " + harbormasterSuccess);
             try {
-                diffClient.sendHarbormasterMessage(phid, harbormasterSuccess);
+                JSONObject result = diffClient.sendHarbormasterMessage(phid, harbormasterSuccess);
+                if (result.containsKey("errorMessage") && !(result.get("errorMessage") instanceof JSONNull)) {
+                    logger.info("harbormaster",
+                            String.format("Error from Harbormaster: %s", result.getString("errorMessage")));
+                    return false;
+                }
             } catch (ArcanistUsageException e) {
-                logger.info("arcanist", "unable to post to sendHarbormasterMessage");
+                logger.info("arcanist", "unable to post to harbormaster");
                 return true;
             }
         } else {

src/main/java/com/uber/jenkins/phabricator/conduit/DifferentialClient.java
@@ -98,7 +98,7 @@ public class DifferentialClient {
         params.put("type", pass ? "pass" : "fail");
         params.put("buildTargetPHID", phid);
 
-        return this.callConduit("sendHarbormasterMessage.sendmessage", params);
+        return this.callConduit("harbormaster.sendmessage", params);
     }
 
     /**